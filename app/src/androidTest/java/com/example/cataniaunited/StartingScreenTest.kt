febc8a49bfa19bdb15afa3abeace40a23774baaa
klavdijaM <klavdija.markovic.dev@gmail.com>
2025-04-06 16:06:09 +0200

    17 - Learn or Start Game page
    - tested StartingScreen buttons behavior when clicked

--- app/src/androidTest/java/com/example/cataniaunited/StartingScreenTest.kt
+++ app/src/androidTest/java/com/example/cataniaunited/StartingScreenTest.kt
@@ -3,8 +3,10 @@ package com.example.cataniaunited
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import com.example.cataniaunited.ui.startingpage.StartingScreen
 import com.example.cataniaunited.ui.theme.CataniaUnitedTheme
+import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 
@@ -23,11 +25,30 @@ class StartingScreenTest {
                 )
             }
         }
-
         composeRule.onNodeWithText("CATAN UNIVERSE").assertIsDisplayed()
         composeRule.onNodeWithText("LEARN").assertIsDisplayed()
         composeRule.onNodeWithText("OR").assertIsDisplayed()
         composeRule.onNodeWithText("START GAME").assertIsDisplayed()
+    }
+
+    @Test
+    fun startingScreenButtonsClickable() {
+        var learnClicked = false
+        var startClicked = false
+
+        composeRule.setContent {
+            CataniaUnitedTheme(darkTheme = false, dynamicColor = false) {
+                StartingScreen(
+                    onLearnClick = { learnClicked = true },
+                    onStartClick = { startClicked = true }
+                )
+            }
+        }
+        composeRule.onNodeWithText("LEARN").performClick()
+        assertTrue(learnClicked)
+
+        composeRule.onNodeWithText("START GAME").performClick()
+        assertTrue(startClicked)
     }
 
 

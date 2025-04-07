d92c48463ba11fe75b7ef2a04f3a42e62da2b8a6
klavdijaM <klavdija.markovic.dev@gmail.com>
2025-04-07 09:04:42 +0200

    11 - Tutorial Page
    -
    - added composable function for headings

--- app/src/main/java/com/example/cataniaunited/ui/tutorial/TutorialScreen.kt
+++ app/src/main/java/com/example/cataniaunited/ui/tutorial/TutorialScreen.kt
@@ -4,11 +4,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -21,6 +26,7 @@ import androidx.compose.ui.unit.dp
 import com.example.cataniaunited.ui.theme.catanGold
 import com.example.cataniaunited.ui.theme.catanRessourceBar
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 
 
 @Composable
@@ -38,7 +44,7 @@ fun TutorialScreen() {
             modifier = Modifier
                 .align(Alignment.Center)
                 .width(430.dp)
-                .heightIn(min = 400.dp, max = 600.dp)
+                .height(400.dp)
                 .border(
                     width = 9.dp,
                     color = catanRessourceBar,
@@ -48,6 +54,43 @@ fun TutorialScreen() {
                     catanGold.copy(alpha = 0.95f),
                     shape = RoundedCornerShape(20.dp)
                 )
+        ) {
+
+            val scrollState = rememberScrollState()
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp)
+                    .verticalScroll(scrollState),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                TutorialHeading("TUTORIAL")
+
+
+        }
+        }
+    }
+}
+
+@Composable
+fun TutorialHeading(title: String){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                catanRessourceBar,
+                shape = RoundedCornerShape(10.dp)
+                )
+            .padding(vertical = 8.dp)
+    ) {
+
+        Text(
+            text = title,
+            color = catanGold,
+            style = MaterialTheme.typography.titleLarge,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+
         )
     }
 }

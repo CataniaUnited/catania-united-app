dcc5c7bcb049f4543920d4da6bacfbc8db1a4cf3
klavdijaM <klavdija.markovic.dev@gmail.com>
2025-04-07 11:10:55 +0200

    11 - Tutorial Page
    - updated the TutorialScreenTest class
    - tested if elements are displayed
    - tested if the back button behaves correctly when clicked

--- app/src/androidTest/java/com/example/cataniaunited/TutorialScreenTest.kt
+++ app/src/androidTest/java/com/example/cataniaunited/TutorialScreenTest.kt
@@ -3,8 +3,10 @@ package com.example.cataniaunited
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import com.example.cataniaunited.ui.theme.CataniaUnitedTheme
 import com.example.cataniaunited.ui.tutorial.TutorialScreen
+import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 
@@ -14,12 +16,41 @@ class TutorialScreenTest {
     val composeRule = createComposeRule()
 
     @Test
-    fun tutorialScreenDisplaysCenterText() {
+    fun testTutorialScreenShowingExpectedText() {
+
         composeRule.setContent {
             CataniaUnitedTheme {
-                TutorialScreen()
+                TutorialScreen(onBackClick = {})
             }
         }
-        composeRule.onNodeWithText("Tutorial Screen").assertIsDisplayed()
+
+        composeRule.onNodeWithText("TUTORIAL").assertIsDisplayed()
+        composeRule.onNodeWithText("BASIC CONCEPTS").assertIsDisplayed()
+        composeRule.onNodeWithText("1.\tSettlements & Cities").assertIsDisplayed()
+        composeRule.onNodeWithText("2.\tRoads").assertIsDisplayed()
+        composeRule.onNodeWithText("3.\tDistance Rule").assertIsDisplayed()
+        composeRule.onNodeWithText("4.\tResource Cards").assertIsDisplayed()
+        composeRule.onNodeWithText("5.\tNumber Tokens & Dice").assertIsDisplayed()
+        composeRule.onNodeWithText("6.\tThe Robber & Rolling a 7").assertIsDisplayed()
+        composeRule.onNodeWithText("7.\tDevelopment Cards").assertIsDisplayed()
+        composeRule.onNodeWithText("TURN SEQUENCE").assertIsDisplayed()
+        composeRule.onNodeWithText("WINNING THE GAME").assertIsDisplayed()
+        composeRule.onNodeWithText("KEY TIPS & NOTES").assertIsDisplayed()
+        composeRule.onNodeWithText("Each player’s turn has 3 main phases:").assertIsDisplayed()
+        composeRule.onNodeWithText("Balancing Resources: If you have too many of one resource").assertIsDisplayed()
+    }
+
+    @Test
+    fun testTutorialScreenBackButtonCallsOnBackClick() {
+        var backButtonClicked = false
+
+        composeRule.setContent {
+            CataniaUnitedTheme {
+                TutorialScreen(onBackClick = { backButtonClicked = true })
+            }
+        }
+
+        composeRule.onNodeWithText("BACK").performClick()
+        assertTrue(backButtonClicked)
     }
 }

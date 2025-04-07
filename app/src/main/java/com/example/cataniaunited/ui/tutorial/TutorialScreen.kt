1060393629efc22224b865f97327f8bc27ba8bca
klavdijaM <klavdija.markovic.dev@gmail.com>
2025-04-07 07:47:28 +0200

    11 - Tutorial Page
    - added background to tutorial page
    - set up the preview

--- app/src/main/java/com/example/cataniaunited/ui/tutorial/TutorialScreen.kt
+++ app/src/main/java/com/example/cataniaunited/ui/tutorial/TutorialScreen.kt
@@ -1,20 +1,41 @@
 package com.example.cataniaunited.ui.tutorial
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cataniaunited.R
+import com.example.cataniaunited.ui.theme.CataniaUnitedTheme
 
 
 @Composable
 fun TutorialScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Tutorial Screen")
+    Box(modifier = Modifier.fillMaxSize()) {
+
+        Image(
+            painter = painterResource(id = R.drawable.catan_starting_page_background),
+            contentDescription = "Tutorial Background",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Preview(
+    name = "Tutorial Screen - Landscape",
+    widthDp = 891,
+    heightDp = 411,
+    showBackground = true
+)
+
+@Composable
+fun TutorialScreenPreview() {
+    CataniaUnitedTheme(darkTheme = false, dynamicColor = false) {
+        TutorialScreen()
     }
 }
 

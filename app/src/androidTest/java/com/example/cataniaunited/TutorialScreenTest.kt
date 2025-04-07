7d26a68cdecdcdf6ecf7b210e1c90be4cae30682
klavdijaM <klavdija.markovic.dev@gmail.com>
2025-04-07 11:32:05 +0200

    11 - Tutorial Page
    - fixed failing tests in TutorialScreenTest
    - added a function to scroll until tested node is in view

--- app/src/androidTest/java/com/example/cataniaunited/TutorialScreenTest.kt
+++ app/src/androidTest/java/com/example/cataniaunited/TutorialScreenTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
 import com.example.cataniaunited.ui.theme.CataniaUnitedTheme
 import com.example.cataniaunited.ui.tutorial.TutorialScreen
 import org.junit.Assert.assertTrue
@@ -24,20 +25,21 @@ class TutorialScreenTest {
             }
         }
 
-        composeRule.onNodeWithText("TUTORIAL").assertIsDisplayed()
-        composeRule.onNodeWithText("BASIC CONCEPTS").assertIsDisplayed()
-        composeRule.onNodeWithText("1.\tSettlements & Cities").assertIsDisplayed()
-        composeRule.onNodeWithText("2.\tRoads").assertIsDisplayed()
-        composeRule.onNodeWithText("3.\tDistance Rule").assertIsDisplayed()
-        composeRule.onNodeWithText("4.\tResource Cards").assertIsDisplayed()
-        composeRule.onNodeWithText("5.\tNumber Tokens & Dice").assertIsDisplayed()
-        composeRule.onNodeWithText("6.\tThe Robber & Rolling a 7").assertIsDisplayed()
-        composeRule.onNodeWithText("7.\tDevelopment Cards").assertIsDisplayed()
-        composeRule.onNodeWithText("TURN SEQUENCE").assertIsDisplayed()
-        composeRule.onNodeWithText("WINNING THE GAME").assertIsDisplayed()
-        composeRule.onNodeWithText("KEY TIPS & NOTES").assertIsDisplayed()
-        composeRule.onNodeWithText("Each player’s turn has 3 main phases:").assertIsDisplayed()
-        composeRule.onNodeWithText("Balancing Resources: If you have too many of one resource").assertIsDisplayed()
+        composeRule.onNodeWithText("TUTORIAL").performScrollTo().assertIsDisplayed()
+        composeRule.onNodeWithText("BASIC CONCEPTS").performScrollTo().assertIsDisplayed()
+        composeRule.onNodeWithText("1.\tSettlements & Cities").performScrollTo().assertIsDisplayed()
+        composeRule.onNodeWithText("2.\tRoads").performScrollTo().assertIsDisplayed()
+        composeRule.onNodeWithText("3.\tDistance Rule").performScrollTo().assertIsDisplayed()
+        composeRule.onNodeWithText("4.\tResource Cards").performScrollTo().assertIsDisplayed()
+        composeRule.onNodeWithText("5.\tNumber Tokens & Dice").performScrollTo().assertIsDisplayed()
+        composeRule.onNodeWithText("6.\tThe Robber & Rolling a 7").performScrollTo().assertIsDisplayed()
+        composeRule.onNodeWithText("7.\tDevelopment Cards").performScrollTo().assertIsDisplayed()
+        composeRule.onNodeWithText("TURN SEQUENCE").performScrollTo().assertIsDisplayed()
+        composeRule.onNodeWithText("WINNING THE GAME").performScrollTo().assertIsDisplayed()
+        composeRule.onNodeWithText("KEY TIPS & NOTES").performScrollTo().assertIsDisplayed()
+        composeRule.onNodeWithText("Each player’s turn has 3 main phases:").performScrollTo().assertIsDisplayed()
+        composeRule.onNodeWithText("o\tBalancing Resources: If you have too many of one resource, try to trade or place a settlement on a special harbor.")
+
     }
 
     @Test

4770d1af83d309472b1888f143bde56662af9c81
matthiasva <matthiasva@edu.aau.at>
2025-04-21 16:52:57 +0200

    #15 - Rolling the dice: - added ViewModel handler for dice roll action

--- app/src/main/java/com/example/cataniaunited/viewmodel/TestPageViewModel.kt
+++ app/src/main/java/com/example/cataniaunited/viewmodel/TestPageViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.cataniaunited.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.cataniaunited.MainApplication
+import com.example.cataniaunited.logic.dto.MessageDTO
+import com.example.cataniaunited.logic.dto.MessageType
 import com.example.cataniaunited.logic.game.GameBoard
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+
 
 class TestPageViewModel : ViewModel() {
     private val gameBoard : GameBoard = GameBoard()
@@ -12,5 +19,9 @@ class TestPageViewModel : ViewModel() {
 
     fun onPlaceRoadClick(roadId: Int, lobbyId: String){
         gameBoard.placeRoad(roadId, lobbyId);
+    }
+
+    fun onDiceRoll(dice1: Int, dice2: Int, lobbyId: String) {
+        gameBoard.rollDice(dice1,dice2,lobbyId)
     }
 }

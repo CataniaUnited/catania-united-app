fb05fc12fb04c5222b3b0c9a879e4d66d8537761
matthiasva <matthiasva@edu.aau.at>
2025-05-04 18:04:33 +0200

    #15 - Rolling the dice: - updated GameBoard to send rollDice action via WebSocket

--- app/src/main/java/com/example/cataniaunited/logic/game/GameBoard.kt
+++ app/src/main/java/com/example/cataniaunited/logic/game/GameBoard.kt
@@ -5,6 +5,7 @@ import com.example.cataniaunited.logic.dto.MessageDTO
 import com.example.cataniaunited.logic.dto.MessageType
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
+import android.util.Log
 
 class GameBoard {
 
@@ -26,14 +27,25 @@ class GameBoard {
         webSocketClient.sendMessage(MessageDTO(MessageType.PLACE_ROAD, playerId, lobbyId, null, message))
     }
 
-    fun rollDice(dice1: Int, dice2: Int, lobbyId: String) {
-        val playerId = MainApplication.getInstance().getPlayerId()
-        val message = buildJsonObject {
-            put("dice1", dice1)
-            put("dice2", dice2)
-            put("Result", dice1+dice2)
+    fun rollDice(lobbyId: String) {
+        try {
+            Log.d("GameBoard", "Rolling dice for lobby: $lobbyId")
+            val playerId = MainApplication.getInstance().getPlayerId()
+            val message = buildJsonObject {
+                put("action", "rollDice")
+            }
+            val webSocketClient = MainApplication.getInstance().getWebSocketClient()
+            webSocketClient.sendMessage(
+                MessageDTO(
+                    MessageType.ROLL_DICE,
+                    playerId,
+                    lobbyId,
+                    null,
+                    message
+                )
+            )
+        } catch (e: Exception) {
+            Log.e("GameBoard", "Error rolling dice", e)
         }
-        val webSocketClient = MainApplication.getInstance().getWebSocketClient()
-        webSocketClient.sendMessage(MessageDTO(MessageType.ROLL_DICE, playerId, lobbyId, null, message))
     }
 }

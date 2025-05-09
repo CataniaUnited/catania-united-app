c8bfcc67553c3c899c147c2a70c8770009a51d9e
matthiasva <matthiasva@edu.aau.at>
2025-05-09 16:10:45 +0200

    #15 - Rolling the dice: - removed unused code and resolved code smells

--- app/src/main/java/com/example/cataniaunited/viewmodel/TestPageViewModel.kt
+++ app/src/main/java/com/example/cataniaunited/viewmodel/TestPageViewModel.kt
@@ -1,18 +1,10 @@
 package com.example.cataniaunited.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.cataniaunited.logic.game.GameBoardLogic
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
 class TestPageViewModel : ViewModel() {
     private val gameBoardLogic : GameBoardLogic = GameBoardLogic()
-
-    private val _diceResult = MutableStateFlow<Pair<Int, Int>?>(null)
-    val diceResult: StateFlow<Pair<Int, Int>?> = _diceResult
 
     fun onPlaceSettlementClick(settlementPositionId: Int, lobbyId: String){
         gameBoardLogic.placeSettlement(settlementPositionId, lobbyId);
@@ -20,16 +12,5 @@ class TestPageViewModel : ViewModel() {
 
     fun onPlaceRoadClick(roadId: Int, lobbyId: String){
         gameBoardLogic.placeRoad(roadId, lobbyId);
-    }
-
-    fun rollDice(lobbyId: String) {
-        gameBoardLogic.rollDice(lobbyId)
-    }
-
-    fun handleDiceResult(dice1: Int, dice2: Int) {
-        Log.d("DiceRoll", "Handling dice result: $dice1, $dice2")
-        viewModelScope.launch {
-            _diceResult.emit(Pair(dice1, dice2))
-        }
     }
 }
